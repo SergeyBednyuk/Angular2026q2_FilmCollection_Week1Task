522e98182fe7f732fc0e9709198634ef45f2738d
Siarhei B <sergeybednyuk@gmail.com>
2026-04-29 18:05:44 +0200

    feat[FilmCollectionApp.app]: add base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FilmsListComponent } from "./films/films-list/films-list.component";
+import { AboutComponent } from "./about/about.component";
+
+export const routes: Routes = [
+    // Home page content
+    { path: '', component: FilmsListComponent},
+    { path: 'about', component: AboutComponent},
+    { path: '**', redirectTo: ''},
+];
