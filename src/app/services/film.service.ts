95f5c74571940d111fac6c708c9a759323120686
Siarhei B <sergeybednyuk@gmail.com>
2026-05-01 17:05:14 +0200

    feat[FilmCollectionApp.film.service]: implement getFilmById, markAsFavorite functionalities

--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -1,18 +1,35 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 
-import { FilmModel} from "../models/film.model";
+import { FilmModel } from "../models/film.model";
 
 @Injectable({
     providedIn: 'root',
 })
 export class FilmService {
-    films = signal<FilmModel[]>([]);
+    private _films = signal<FilmModel[]>([]);
+    films = this._films.asReadonly();
+    favoriteFilms = computed(() => this._films().filter((f) => f.isFavorite));
+    private _isLoading = signal(true);
+    isLoading = this._isLoading.asReadonly();
 
     constructor() {
-        this.loadFilms().then(result => this.films.set(result));
+        this.loadFilms().then(result => {
+            this._films.set(result);
+            this._isLoading.set(false);
+        });
     }
 
+    getFilmById(filmId: number): FilmModel | undefined {
+        return this._films().find((film) => film.id === filmId);
+    }
 
+    markAsFavorite(filmId: number): void {
+        this._films.update((films) => {
+            return films.map((film) =>
+                film.id === filmId ? { ...film, isFavorite: !film.isFavorite } : film
+            );
+        });
+    }
 
     private async loadFilms(): Promise<FilmModel[]> {
         try {
