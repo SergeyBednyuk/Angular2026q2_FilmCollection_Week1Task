91959e13e0aa08de41d4723b720e553fb181d017
Siarhei B <sergeybednyuk@gmail.com>
2026-05-01 17:03:49 +0200

    feat[FilmCollectionApp.films-list]: add base implementation for films list

--- src/app/films/films-list/films-list.component.ts
+++ src/app/films/films-list/films-list.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FilmService } from "../../services/film.service";
+import { FilmItemComponent } from "../film-item/film-item.component";
 
 @Component({
-  selector: 'app-films-list',
-  imports: [],
-  templateUrl: './films-list.component.html',
-  styleUrl: './films-list.component.css',
+    selector: 'app-films-list',
+    imports: [
+        FilmItemComponent
+    ],
+    templateUrl: './films-list.component.html',
+    styleUrl: './films-list.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FilmsListComponent {}
+export class FilmsListComponent {
+    private filmsService = inject(FilmService);
+    protected films = this.filmsService.films;
+    protected isLoading = this.filmsService.isLoading;
+}
