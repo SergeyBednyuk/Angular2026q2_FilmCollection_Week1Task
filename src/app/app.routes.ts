1eff33099490243a5b73d54cc959439e8e17244a
Siarhei B <sergeybednyuk@gmail.com>
2026-05-05 15:14:57 +0200

    feat[FilmCollectionApp.breadcrumbs]: implement breadcrumbs functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { FilmsListComponent } from "./films/films-list/films-list.component";
 import { AboutComponent } from "./about/about.component";
+import { FilmDetailsComponent } from "./films/film-details/film-details.component";
 
 export const routes: Routes = [
     // Home page content
-    { path: '', component: FilmsListComponent},
-    { path: 'about', component: AboutComponent},
-    { path: '**', redirectTo: ''},
+    { path: '', component: FilmsListComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'films/:id', component: FilmDetailsComponent },
+    { path: '**', redirectTo: '' },
 ];
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from "@angular/router";
+
+import { FilmService } from "../../services/film.service";
+import { BreadcrumbModel } from "../../models/breadcrumb.model";
+
 
 @Component({
-  selector: 'app-breadcrumbs',
-  imports: [],
-  templateUrl: './breadcrumbs.component.html',
-  styleUrl: './breadcrumbs.component.css',
+    selector: 'app-breadcrumbs',
+    imports: [
+        RouterLink
+    ],
+    templateUrl: './breadcrumbs.component.html',
+    styleUrl: './breadcrumbs.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BreadcrumbsComponent {}
+export class BreadcrumbsComponent {
+    private destroyRef = inject(DestroyRef);
+    private router = inject(Router);
+    private currentUrl = signal(this.router.url);
+    private filmService = inject(FilmService);
+
+    constructor() {
+        const subscription = this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.currentUrl.set(event.urlAfterRedirects);
+            }
+        });
+        this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    }
+
+    protected crumbs = computed<BreadcrumbModel[]>(() => {
+        const url = this.currentUrl();
+
+        if (url === '/') return [{ label: 'Home', url: url }];
+        if (url === '/about') return [{ label: 'About', url: url }];
+        if (url.startsWith('/films/')) {
+            const id = url.split('/')[2];
+            const film = this.filmService.getFilmById(Number(id));
+            const label = film?.title ?? 'Film';
+            return [{ label: 'Home', url: '/' }, { label: label, url: url }];
+        }
+
+        return [];
+    });
+
+}
